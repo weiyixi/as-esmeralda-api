8c7fb4c1aec8c2c468c7ea321dde8e7e3e017603
yzhu2 <yzhu2@i9i8.com>
2014-06-27 10:08:04 +0800

    add order service

--- src/esmeralda_service/order/DbOrderService.php
+++ src/esmeralda_service/order/DbOrderService.php
@@ -1,9 +1,43 @@
 <?php
 namespace esmeralda_service\order;
 
-class DbOrderService implements OrderService {
+/**
+ * order service that query directly from DB
+ **/
+class DbOrderService implements OrderService
+{
+    protected $dao;
+    protected $domain;
+
+    public function __construct($dao, $domain){
+        $this->dao = $dao;
+        $this->domain = $domain;
+    }
+
     public function getOrder(){
         echo "\nservice - get order";
     }
+
+    public function getOrderInfo($orderSn, $userId = null) {
+        return $this->dao->getOrderInfo($orderSn, $userId);
+    }
+
+    public function getOrderGoods($orderIds = null, $sku = null) {
+        return $this->dao->getOrderGoods($orderIds, $sku);
+    }
+
+    public function getOrderGoodsByIds($recIds) {
+        return $this->dao->getOrderGoodsByIds($recIds);
+    }
+
+    public function getOrderExtension($orderId) {
+        return $this->dao->getOrderExtension($orderId);
+    }
+
+    public function getOrderCopyLog($orderSn) {
+        return $this->dao->getOrderCopyLog($orderSn);
+    }
+
 }
+
 
--- src/esmeralda_service/order/OrderService.php
+++ src/esmeralda_service/order/OrderService.php
@@ -3,5 +3,10 @@ namespace esmeralda_service\order;
 
 interface OrderService{
     public function getOrder();
+    public function getOrderInfo($orderSn, $userId = null);
+    public function getOrderGoods($orderIds = null, $sku = null);
+    public function getOrderGoodsByIds($recIds);
+    public function getOrderExtension($orderId);
+    public function getOrderCopyLog($orderSn);
 }
 
