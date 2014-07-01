fc90a1d09f4b433a2c106a434ea072c94c863856
hywang <hywang@i9i8.com>
2014-07-01 15:44:49 +0800

    update raworder api

--- src/sync-api/raworder.rest.php
+++ src/sync-api/raworder.rest.php
@@ -9,7 +9,6 @@ $prefix = '/sync-apis/raworder';
 $logger = ApiLogFactory::get('raworder.rest');
 
 $container['slim']->post($prefix.'/post/:domain', function ($domain) use ($container) {
-    $slim = $container['slim'];
 
 	$response = array('code' => 0, 'msg' => '', 'data' => array());
 	$jsonTpl = 'json.tpl';
@@ -46,11 +45,11 @@ $container['slim']->post($prefix.'/post/:domain', function ($domain) use ($conta
 	} while ($try < 10);
 	$_POST['order_info']['order_sn'] = $orderSn;
 
-	// insert goods sku
+	// insert goods sku items
 	$skuIds = array();
 	if (isset($_POST['goods_sku']) && is_array($_POST['goods_sku'])) {
 		$container['orderW']->beginTransaction();
-		foreach ($_POST['goods_sku'] as $oldSkuId=>$skuItems) {
+		foreach ($_POST['goods_sku'] as $oldRecId=>$skuItems) {
 			$skuId = $container['order']->checkSkuIdExists($skuItems);
 			if (!$skuId) {
 				$skuId = $container['orderW']->insert('goods_sku', $skuItems);
@@ -62,7 +61,7 @@ $container['slim']->post($prefix.'/post/:domain', function ($domain) use ($conta
 					die;
 				}
 			}
-			$skuIds[$oldSkuId] = $skuId;
+			$skuIds[$oldRecId] = $skuId;
 		}
 		$res = $container['orderW']->commit();
 		if (!$res) {
@@ -73,21 +72,33 @@ $container['slim']->post($prefix.'/post/:domain', function ($domain) use ($conta
 		}
 	}
 
+	// create sku
+	$skus = array();
+	foreach ($_POST['order_goods'] as $oldRecId=>$orderGoods) {
+		$sku = $orderGoods['sku'];
+		$sku = preg_replace('/(\d*)(g\d+)([a-fh-z]?)/', '\1g'.$orderGoods['goods_id'].'\3', $sku);
+		if (isset($skuIds[$oldRecId])) {
+			$sku = preg_replace('/(\d*)(z\d+)([a-y]?)/', '\1z'.$skuIds[$oldRecId].'\3', $sku);
+		}
+		$skus[$oldRecId] = $sku;
+	}
+
 	// insert goods style
 	$goodsStyleIds = array();
 	if (isset($_POST['goods_style']) && is_array($_POST['goods_style']) && !empty($skuIds)) {
 		$container['orderW']->beginTransaction();
-		foreach ($_POST['goods_style'] as $oldGStyleId => $styleItems) {
-			if (!isset($styleItems['sku_id']) || !isset($skuIds[$styleItems['sku_id']])) {
+		foreach ($_POST['goods_style'] as $oldRecId => $styleItems) {
+			// sku can not be empty
+			if (!isset($skus[$oldRecId]) || empty($skus[$oldRecId])) {
 				continue;
 			}
 			// replace old skuId with new skuId
-			$styleItems['sku_id'] = $skuIds[$styleItems['sku_id']];
-			// create new sku
-			$styleItems['sku'] = preg_replace('/(\d*)(z\d+)([a-y]?)/', '\1z'.$styleItems['sku_id'].'\3', $styleItems['sku']);
-			$styleItems['sku'] = preg_replace('/(\d*)(g\d+)([a-fh-z]?)/', '\1g'.$styleItems['goods_id'].'\3', $styleItems['sku']);
+			$styleItems['sku_id'] = isset($skuIds[$oldRecId]) ? $skuIds[$oldRecId] : 0;
+			// replace old sku with new sku
+			$styleItems['sku'] = $skus[$oldRecId];
 			$gStyleId = $container['order']->checkGStyleIdExists($styleItems);
 			if (!$gStyleId) {
+				$styleItems['style_price'] = $_POST['order_goods'][$oldRecId]['shop_price'];
 				$gStyleId = $container['orderW']->insert('goods_style', $styleItems);
 				if (!$gStyleId) {
 					$container['orderW']->rollBack();
@@ -97,7 +108,7 @@ $container['slim']->post($prefix.'/post/:domain', function ($domain) use ($conta
 					die;
 				}
 			}
-			$goodsStyleIds[$oldGStyleId] = $gStyleId;
+			$goodsStyleIds[$oldRecId] = $gStyleId;
 		}
 		$res = $container['orderW']->commit();
 		if (!$res) {
@@ -108,7 +119,7 @@ $container['slim']->post($prefix.'/post/:domain', function ($domain) use ($conta
 		}
 	}
 
-	// insert goods style id
+	// insert order_info
 	$container['orderW']->beginTransaction();
 	$affectedRows = $container['orderW']->insert('order_info', array($_POST['order_info']));
 	if (!$affectedRows) {
@@ -117,15 +128,32 @@ $container['slim']->post($prefix.'/post/:domain', function ($domain) use ($conta
 	    $slim->render($jsonTpl, array('value' => $response, 'json_format' => $jsonFormat));
 		die;
 	}
-	$affectedRows = $container['orderW']->insert('order_goods', $_POST['order_goods']);
-	if (!$affectedRows) {
-		$container['orderW']->rollBack();
-		$response['msg'] = "insert goods info failed.";
-		$logger->error($response['msg']);
-	    $slim->render($jsonTpl, array('value' => $response, 'json_format' => $jsonFormat));
-		die;
+	$orderId = $container['orderW']->getLastInsertId();
+	// insert order_goods
+	$recIds = array();
+	foreach ($_POST['order_goods'] as $oldRecId=>$orderGoods) {
+		$orderGoods['order_id'] = $orderId;
+		$orderGoods['goods_style_id'] = $goodsStyleIds[$oldRecId];
+		$orderGoods['sku'] = $skus[$oldRecId];
+		$orderGoods['sku_id'] = isset($skuIds[$oldRecId]) ? $skuIds[$oldRecId] : 0;
+		$affectedRows = $container['orderW']->insert('order_goods', $_POST['order_goods']);
+		if (!$affectedRows) {
+			$container['orderW']->rollBack();
+			$response['msg'] = "insert order goods failed.";
+			$logger->error($response['msg']);
+		    $slim->render($jsonTpl, array('value' => $response, 'json_format' => $jsonFormat));
+			die;
+		}
+		$newRecId = $container['orderW']->getLastInsertId();
+		$recIds[$oldRecId] = $newRecId;
 	}
+	// insert order_extension
 	if (isset($_POST['order_extension']) && !empty($_POST['order_extension'])) {
+		array_push($_POST['order_extension'], array(
+			'order_id' => $orderId,
+			'ext_name' => 'newRecIds',
+			'ext_value' => json_encode($recIds),
+		));
 		$affectedRows = $container['orderW']->insert('order_extension', array($_POST['order_extension']));
 		if (!$affectedRows) {
 			$container['orderW']->rollBack();
@@ -150,14 +178,13 @@ $container['slim']->post($prefix.'/post/:domain', function ($domain) use ($conta
 	$slim->render($jsonTpl, array('value' => $response, 'json_format' => $jsonFormat));
 });
 
-$container['slim']->post($prefix.'/pay', function () use ($container) {
-    $slim = $container['slim'];
+$container['slim']->post($prefix.'/pay/:orderSn', function ($orderSn) use ($container) {
+
 	$response = array('code' => 0, 'msg' => '', 'data' => array());
 	$jsonTpl = 'json.tpl';
 	$paid = 2;
-	$orderSn = $_POST['orderSn'];
 
-	if (!isset($orderSn)) {
+	if (empty($orderSn)) {
 		$response['msg'] = "missing order sn.";
 		$logger->error($response['msg']);
 	    $slim->render($jsonTpl, array('value' => $response, 'json_format' => JSON_FORCE_OBJECT | JSON_PRETTY_PRINT));
